ee0003c52bc356227bd5fe3e274635cc5de93754
ellasimm <ella.josephine.simm@gmail.com>
2023-03-28 19:28:44 -0400

    LIST IS POPULATED just need to fix UI

--- app/src/main/java/com/example/notes/NotesActivity.java
+++ app/src/main/java/com/example/notes/NotesActivity.java
@@ -1,33 +1,163 @@
 package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class NotesActivity extends AppCompatActivity {
+
+    ArrayList<Note> notes;
+    notesAdapter notesAdapter = new notesAdapter(notes, this);
+    RecyclerView noteList;
+
+    private View.OnClickListener onItemClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            RecyclerView.ViewHolder viewHolder = (RecyclerView.ViewHolder) view.getTag();
+            int position = viewHolder.getAdapterPosition();
+            int noteId = notes.get(position).getNoteID();
+            Intent intent = new Intent(NotesActivity.this, MainActivity.class);
+            intent.putExtra("noteID", noteId);
+            startActivity(intent);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        initListButton();
+        initSettingsButton();
+        initAddContactButton();
+        initDeleteSwitch();
+        notesAdapter.setOnItemClickListener(onItemClickListener);
+        String sortBy = getSharedPreferences("MyNoteListPreferences",
+                Context.MODE_PRIVATE).getString("sortfield", "title");
+        String sortOrder = getSharedPreferences("MyNoteListPreferences",
+                Context.MODE_PRIVATE).getString("sortorder", "ASC");
 
-        initSettings();
+
+        NotesDBHelper ds = new NotesDBHelper(this);
+
+        try {
+            ds.open();
+            notes = ds.getAllNotes(sortBy, sortOrder);
+            ds.close();
+
+            RecyclerView noteList = findViewById(R.id.rvNotes);
+
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+            noteList.setLayoutManager(layoutManager);
+
+            notesAdapter noteAdapter = new notesAdapter(notes, this);
+
+            noteList.setAdapter(noteAdapter);
+
+
+        } catch (Exception e) {
+            Toast.makeText(this, "Error retrieving Notes", Toast.LENGTH_LONG).show();
+        }
+
+//        BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context context, Intent intent) {
+//                double batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+//                double levelScale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 0);
+//                int batteryPercent  = (int) Math.floor(batteryLevel / levelScale * 100);
+//                TextView textBatteryState  = findViewById(R.id.textBatteryLevel);
+//                textBatteryState.setText(batteryPercent + "%");
+//            }
+//        };
+//
+//        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+//        registerReceiver(batteryReceiver, filter);
+
     }
 
-    private void initSettings() {
-        ImageButton ibList = findViewById(R.id.settingsButton);
-        ibList.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onResume() {
+        super.onResume();
+        String sortBy = getSharedPreferences("MyNoteListPreferences",
+                Context.MODE_PRIVATE).getString("sortfield", "title");
+        String sortOrder = getSharedPreferences("MyNoteListPreferences",
+                Context.MODE_PRIVATE).getString("sortorder", "ASC");
+        NotesDBHelper ds = new NotesDBHelper(this);
+        try{
+            ds.open();
+            notes = ds.getAllNotes(sortBy, sortOrder);
+            ds.close();
+            if (notes.size() > 0 ) {
+                noteList = findViewById(R.id.rvNotes);
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+                noteList.setLayoutManager(layoutManager);
+                notesAdapter = new notesAdapter(notes, this);
+                noteList.setAdapter(notesAdapter);
+            } else {
+                Intent intent = new Intent(NotesActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Error Retrieving Notes", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void initDeleteSwitch() {
+        Switch s = findViewById(R.id.switchDelete);
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Boolean status = compoundButton.isChecked();
+                notesAdapter.setDelete(status);
+                notesAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
+    private void initListButton() {
+    }
+
+
+    private void initSettingsButton() {
+        ImageButton ibSettings = findViewById(R.id.settingsButton);
+        ibSettings.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(NotesActivity.this, SettingsActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
         });
+
     }
 
-    private void initNotes() {
+    private void initAddContactButton() {
+        Button newContact = findViewById(R.id.buttonAddContact);
+        newContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotesActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/notes/notesAdapter.java
+++ app/src/main/java/com/example/notes/notesAdapter.java
@@ -1,4 +1,125 @@
 package com.example.notes;
 
-public class notesAdapter {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class notesAdapter extends RecyclerView.Adapter {
+    private ArrayList<Note> noteData;
+    private static View.OnClickListener mOnItemClickListener;
+    private boolean isDeleting;
+    private Context parentContext;
+
+    public notesAdapter(ArrayList<Note> arrayList, Context context) {
+        noteData = arrayList;
+        parentContext = context;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        NoteViewHolder cvh = (NoteViewHolder) holder;
+        cvh.getTextTitle().setText(noteData.get(position).getTitle());
+        cvh.getTextDescription().setText(noteData.get(position).getDescription());
+        cvh.getTextDate().setText(noteData.get(position).getDate() + ",");
+        if (isDeleting) {
+            cvh.getDeleteButton().setVisibility(View.VISIBLE);
+            cvh.getDeleteButton().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    deleteItem(position);
+                }
+            });
+        }
+        else {
+            cvh.getDeleteButton().setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void setDelete(boolean b) {
+        isDeleting = b;
+    }
+
+    private void deleteItem(int position){
+        Note note = noteData.get(position);
+        NotesDBHelper ds = new NotesDBHelper(parentContext);
+        try {
+            ds.open();
+            boolean didDelete = ds.deleteNote(note.getNoteID());
+            ds.close();
+            if (didDelete) {
+                noteData.remove(position);
+                notifyDataSetChanged();
+            } else {
+                Toast.makeText(parentContext, "Delete Failed!", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(parentContext, "Delete Failed!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textTitle;
+        public TextView textDescription;
+        public Button deleteButton;
+        public TextView textDate;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textDescription = itemView.findViewById(R.id.textDescription);
+            textDate = itemView.findViewById(R.id.textDueDate);
+            deleteButton = itemView.findViewById(R.id.buttonDeleteContact);
+            itemView.setTag(this);
+            itemView.setOnClickListener(mOnItemClickListener);
+        }
+
+        public Button getDeleteButton() {
+            return deleteButton;
+        }
+
+        public TextView getTextTitle() {
+
+            return textTitle;
+        }
+
+        public TextView getTextDescription() {
+
+            return textDescription;
+        }
+        public TextView getTextDate() {
+
+            return textDate;
+        }
+    }
+
+
+    public static void setOnItemClickListener(View.OnClickListener itemClickListener) {
+        mOnItemClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new NoteViewHolder(v);
+    }
+
+
+    @Override
+    public int getItemCount () {
+
+        return noteData.size();
+    }
 }
